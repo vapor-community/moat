dfd4fcacda8ccd38360909e077b6577f68b384f2
cakinney <cakinney@gmail.com>
2018-06-05 06:43:15 -0400

    SPM Update
    Added Leaf dependency + Fixes

--- Package.swift
+++ Package.swift
@@ -4,10 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "Moat",
+    products: [
+        .library(name: "Moat", targets: ["Moat"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        ],
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
+    ],
     targets: [
-        .target(name: "Moat", dependencies: ["Vapor"]),
+        .target(name: "Moat", dependencies: ["Vapor", "Leaf"]),
     ]
 )
